c79f9f4215e329e3eeb384151f2b61977337ebdb
skurdenis <skurdenis@yandex.ru>
2018-12-05 22:07:53 +0300

    Update TwoMetrics.java

--- TwoMetrics.java
+++ TwoMetrics.java
@@ -1,16 +1,15 @@
 package com.dreamwork.art.service.converters;
+
 import com.dreamwork.art.model.Metric;
 import com.dreamwork.art.service.Converter;
 
 import java.util.*;
 
 
-
-
 public class TwoMetrics implements Converter {
     private static int countMatches(String str, String sub) {
         int count = 0;
-        if(!str.isEmpty() && !sub.isEmpty()) {
+        if (!str.isEmpty() && !sub.isEmpty()) {
             for (int i = 0; (i = str.indexOf(sub, i)) != -1; i += sub.length()) {
                 count++;
             }
@@ -20,30 +19,30 @@ public class TwoMetrics implements Converter {
 
     @Override
     public List<Metric> convert(LinkedHashMap projectData) {
-        String data =  projectData.get("data").toString();
+        String data = projectData.get("data").toString();
 
 
-        int count=0;
-        int count1=0;
-        int countmile=0;
-        int countclosedmile=0;
-        count=countMatches(data, "closed=true");
-        count1=countMatches(data, "closed=false")+count;
-        countmile=countMatches(data, "dueOn");
-        countclosedmile=countMatches(data, "state=CLOSED");
-        float ans=0;
-        if(count1==0) ans=1; else ans=(float) count/count1;
-        float toreturnprogress=0;
-        if (countmile>0 )toreturnprogress=ans*(countclosedmile+1)/countmile;
-        else  toreturnprogress=ans;
-        float toreturncount=count1;
-      
-    
-        
-        Metric test1=new Metric("progress", toreturnprogress);
-        Metric test2=new Metric("countclosed", toreturncount);
-        List<Metric> toreturn=new ArrayList<>();
-        Collections.addAll(toreturn, test1,test2);
+        int count = 0;
+        int count1 = 0;
+        int countmile = 0;
+        int countclosedmile = 0;
+        count = countMatches(data, "closed=true");
+        count1 = countMatches(data, "closed=false") + count;
+        countmile = countMatches(data, "dueOn");
+        countclosedmile = countMatches(data, "state=CLOSED");
+        float ans = 0;
+        if (count1 == 0) ans = 1;
+        else ans = (float) count / count1;
+        float toreturnprogress = 0;
+        if (countmile > 0) toreturnprogress = ans * (countclosedmile + 1) / countmile;
+        else toreturnprogress = ans;
+        float toreturncount = count1;
+
+
+        Metric test1 = new Metric("progress", toreturnprogress);
+        Metric test2 = new Metric("countclosed", toreturncount);
+        List<Metric> toreturn = new ArrayList<>();
+        Collections.addAll(toreturn, test1, test2);
         return toreturn;
 
     }
@@ -51,7 +50,7 @@ public class TwoMetrics implements Converter {
     @Override
     public Set<String> types() {
 
-        Set<String> s=new HashSet<String>() {{
+        Set<String> s = new HashSet<String>() {{
             add("progress");
             add("countclosed");
         }};
@@ -59,5 +58,4 @@ public class TwoMetrics implements Converter {
     }
 
 }
-
 
